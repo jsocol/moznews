f11f42a83fec53a34ae9880727b8ad322ffff50d
James Socol <me@jamessocol.com>
2013-02-15 18:35:37 -0500

    Limit to non-replies.

--- moznews/news/views.py
+++ moznews/news/views.py
@@ -4,4 +4,5 @@ from moznews.news.models import Item
 
 
 def main(request):
-    items = Item.scored().order_by('-score')
+    items = Item.scored().filter(in_reply_to__isnull=True).order_by('-score')
+    render(request, 'main.html', {'items': items})
